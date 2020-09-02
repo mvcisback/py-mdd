f89dda3479371208a80f0a4e81ce4926631a930a
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-09-01 22:19:07 -0700

    Implement Variable type.

--- mdd/mdd.py
+++ mdd/mdd.py
@@ -1,5 +1,12 @@
-from typing import Any, Callable, Dict, Generic, Hashable, Sequence, Union
+from __future__ import annotations
 
+import math
+from typing import Any, Callable, Dict, Generic, Hashable
+from typing import Iterable, Sequence, Union
+
+import aiger
+import aiger_bv
+import aiger_bdd
 import attr
 
 
@@ -9,26 +16,56 @@ BDD = Any
 
 @attr.s(frozen=True, auto_attribs=True)
 class Variable:
+    """BDD representation of a multi-valued variable."""
     name: Hashable
-    valid: BDD
-    encode: Callable[[Hashable], Assignment]
-    decode: Callable[[Assignment], Hashable]
+    size: int
+    valid: aiger_bv.SignedBVExpr
+    encode: Callable[[Hashable], int]
+    decode: Callable[[int], Hashable]
 
 
 VariableLike = Union[Sequence[Hashable], Variable]
 
 
-def to_var(vals: VariableLike) -> Variable:
-    pass
+def pow2_exponent(val: int) -> int:
+    """Compute exponent of power of 2."""
+    count = 0
+    while val > 1:
+        count += 1
+        val >>= 1
+    assert val == 1
+    return count
 
+
+def to_bdd(circ_or_expr) -> BDD:
+    return aiger_bdd.to_bdd(circ_or_expr, renamer=lambda _, x: x)[0]
+
+
+def to_var(vals: Iterable[Hashable], name: str) -> Variable:
+    """Create BDD representation of a variable taking on values in `vals`."""
+    vals = tuple(vals)
+    tmp = aiger_bv.atom(len(vals), name, signed=True)
+
+    # Create circuit representing classic onehot bit trick.
+    one_hot = (tmp != 0) & ((tmp & (tmp - 1)) == 0)
+
+    return Variable(
+        name=name,
+        size=len(vals),
+        valid=one_hot.with_output("valid"),
+        encode=lambda val: 1 << vals.index(val),
+        decode=lambda val: vals[pow2_exponent(val)],
+    )
 
 
 @attr.s(frozen=True, auto_attribs=True)
 class Interface:
+    """Input output interface of Multi-valued Decision Diagram."""
     inputs: Sequence[Variable]
     output: Variable
 
     def lift(self, val: ValueLike) -> MDD:
+        
         pass
 
     def ite(self, test: ValueLike, pos: MDD, neg: MDD) -> MDD:
@@ -44,4 +81,4 @@ class MDD:
 ValueLike = Union[Assignment, BDD, MDD]
 
 
-__all__ = ["MDD", "Interface", "Variable", "BDD"]
+__all__ = ["MDD", "Interface", "Variable", "BDD", "to_var"]
---
+++ tests/test_mdd.py
@@ -0,0 +1,23 @@
+import mdd
+
+
+def test_to_var():
+    var = mdd.to_var(vals=["x", "y", "z"], name="myvar")
+    assert var.name == "myvar"
+    assert var.size == 3
+
+    assert var.encode("x") == 0b001
+    assert var.encode("y") == 0b010
+    assert var.encode("z") == 0b100
+
+    assert var.decode(0b100) == 'z'
+    assert var.decode(0b010) == 'y'
+    assert var.decode(0b001) == 'x'
+
+    # Test valid circuit checks onehot.
+
+    valid_input = {'myvar': (True, False, False)}
+    invalid_input = {'myvar': (True, True, False)}
+    assert var.valid(valid_input)[0]
+    assert not var.valid(invalid_input)[0]
+
